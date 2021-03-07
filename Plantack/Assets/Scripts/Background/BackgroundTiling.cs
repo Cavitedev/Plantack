31912110bf3c53e769ea3681bea4374090f4da36
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-03-07 11:56:51 +0100

    :poop: Add tiling background

--- Plantack/Assets/Scripts/Background/BackgroundTiling.cs
+++ Plantack/Assets/Scripts/Background/BackgroundTiling.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
-public class BackgroundTiling : MonoBehaviour
+namespace Background
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BackgroundTiling : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Transform target;
+        [SerializeField] private Transform[] backgrounds;
+        [SerializeField] private float size = 25;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private float _spaceToUpdateBackground;
+        private float _threshold = 1;
+        private int _firstBackgroundIndex = 0;
+        private int LastBackgroundIndex => _firstBackgroundIndex > 0 ? _firstBackgroundIndex - 1 : backgrounds.Length - 1;
+
+
+
+        private void Start()
+        {
+            Debug.Assert(Camera.main != null, "Camera.main != null");
+            Camera mainCamera = Camera.main;
+            _spaceToUpdateBackground = mainCamera.orthographicSize * mainCamera.aspect + _threshold;
+        }
+
+        private void Update()
+        {
+            float targetX = target.position.x;
+            float firstBackgroundXPos = backgrounds[_firstBackgroundIndex].position.x;
+            float lastBackgroundXPos = backgrounds[LastBackgroundIndex].position.x;
+            if (firstBackgroundXPos - size / 2 > targetX - _spaceToUpdateBackground)
+            {
+                Vector3 pos = backgrounds[LastBackgroundIndex].position;
+                backgrounds[LastBackgroundIndex].position = new Vector3(firstBackgroundXPos - size, pos.y, pos.z);
+                _firstBackgroundIndex = LastBackgroundIndex;
+            }else if (lastBackgroundXPos + size / 2 < targetX + _spaceToUpdateBackground)
+            {
+                Vector3 pos = backgrounds[_firstBackgroundIndex].position;
+                backgrounds[_firstBackgroundIndex].position = new Vector3(firstBackgroundXPos + size, pos.y, pos.z);
+                _firstBackgroundIndex +=1 ;
+                _firstBackgroundIndex %= backgrounds.Length;
+            }
+        }
     }
 }
