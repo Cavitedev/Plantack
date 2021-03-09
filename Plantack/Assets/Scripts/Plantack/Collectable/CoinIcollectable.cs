1b55e12c51040c5600b5d7cab87264ec9eb422c9
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-03-09 15:28:09 +0100

    :sparkles: Adding coins sounds

--- Plantack/Assets/Scripts/Plantack/Collectable/CoinIcollectable.cs
+++ Plantack/Assets/Scripts/Plantack/Collectable/CoinIcollectable.cs
@@ -1,22 +1,26 @@
 ﻿using System;
-using Plantack.Interactable;
+using Plantack.Management;
 using Plantack.Player;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Plantack.Collectable
 {
-    [RequireComponent(typeof(Collider2D), typeof(AudioSource))]
+    [RequireComponent(typeof(Collider2D))]
     public class CoinIcollectable : MonoBehaviour, ICollectable
     {
         [SerializeField] private int value = 1;
-        [SerializeField] private AudioSource sound;
+        [SerializeField] private AudioClip sound;
+        [SerializeField] private float pitch = 1;
+        [SerializeField] private float randomPitchAdd = 0.01f;
         
 
         public void Collect(PlayerStats playerStats)
         {
             playerStats.Coins += value;
-            sound.Play();
-            Destroy(gameObject, sound.clip.length);
+            float currentPitch = pitch + Random.Range(-1, 1) * randomPitchAdd;
+            AudioManager.instance.PlaySound(sound, transform.position, currentPitch);
+            Destroy(gameObject);
             
         }
     }
---
+++ Plantack/Assets/Scripts/Plantack/Management/AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plantack.Management
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [SerializeField] private int startAmountSounds;
+        [SerializeField] private GameObject audioGameObject;
+        
+        public static AudioManager instance;
+        private List<AudioSource> _audioSourcesPool = new List<AudioSource>();
+        
+        
+        private void Awake()
+        {
+            if(instance != null)
+                Destroy(gameObject);
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            for (int i = 0; i < startAmountSounds; i++)
+            {
+                InstantiateAudioSource(i);
+            }
+        }
+
+
+
+        public void PlaySound(AudioClip clip, Vector3 pos, float pitch = 1.0f, bool loop = false)
+        {
+            AudioSource audioSource = FirstAvailableAudioSource();
+            audioSource.clip = clip;
+            audioSource.transform.position = pos;
+            audioSource.pitch = pitch;
+            audioSource.loop = loop;
+            audioSource.Play();
+        }
+
+        private AudioSource InstantiateAudioSource(int i)
+        {
+            GameObject audioSourceGO = Instantiate(audioGameObject, transform);
+            audioSourceGO.name = "Audio Source " + i;
+            AudioSource audioSource = audioSourceGO.GetComponent<AudioSource>();
+            _audioSourcesPool.Add(audioSource);
+            return audioSource;
+        }
+        
+        private AudioSource FirstAvailableAudioSource()
+        {
+            foreach (AudioSource audioSource in _audioSourcesPool)
+            {
+                if (!audioSource.isPlaying)
+                {
+                    return audioSource;
+                }
+            }
+
+            return InstantiateAudioSource(_audioSourcesPool.Count);
+        }
+        
+        
+        
+    }
+}
