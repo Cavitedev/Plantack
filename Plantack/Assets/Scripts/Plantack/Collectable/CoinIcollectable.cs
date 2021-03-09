ee62e48b710ca4b152c358c5fc47ada037950a3f
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-03-09 14:32:41 +0100

    :construction: Working on coin sounds

--- Plantack/Assets/Scripts/Plantack/Collectable/CoinIcollectable.cs
+++ Plantack/Assets/Scripts/Plantack/Collectable/CoinIcollectable.cs
@@ -1,20 +1,23 @@
-﻿using Plantack.Interactable;
+﻿using System;
+using Plantack.Interactable;
 using Plantack.Player;
 using UnityEngine;
 
 namespace Plantack.Collectable
 {
-    [RequireComponent(typeof(Collider2D))]
+    [RequireComponent(typeof(Collider2D), typeof(AudioSource))]
     public class CoinIcollectable : MonoBehaviour, ICollectable
     {
-
         [SerializeField] private int value = 1;
+        [SerializeField] private AudioSource sound;
         
-        
+
         public void Collect(PlayerStats playerStats)
         {
             playerStats.Coins += value;
-            Destroy(gameObject);
+            sound.Play();
+            Destroy(gameObject, sound.clip.length);
+            
         }
     }
 }
