7cc88e458430a22f011e3166714b61df21572c2d
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-03-11 18:05:09 +0100

    :beers: Enemy movement

---
+++ Plantack/Assets/Scripts/Plantack/Enemy/EnemyStats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Plantack.Collectable;
+using Plantack.Player;
+using UnityEngine;
+
+namespace Plantack.Enemy
+{
+    [RequireComponent(typeof(SimpleEnemyMovement))]
+    public class EnemyStats : MonoBehaviour, ICollectable
+    {
+
+        [SerializeField] private float damage;
+
+        private SimpleEnemyMovement _enemyMovement;
+
+        private void Start()
+        {
+            _enemyMovement = GetComponent<SimpleEnemyMovement>();
+        }
+
+        public void Collect(PlayerStats playerStats)
+        {
+            _enemyMovement.Stop();
+            playerStats.Health -= damage;
+        }
+    }
+}
