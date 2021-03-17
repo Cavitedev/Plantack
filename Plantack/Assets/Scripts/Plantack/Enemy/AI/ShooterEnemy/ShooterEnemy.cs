a999ce975394cb5ffc2a04161ae2909dbb4027ab
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-03-17 13:32:59 +0100

    :recycle: Move and rename shooterEnemy

--- Plantack/Assets/Scripts/Plantack/Enemy/AI/ShooterEnemy/ShooterEnemy.cs
+++ Plantack/Assets/Scripts/Plantack/Enemy/AI/ShooterEnemy/ShooterEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class ShooterEnemy : MonoBehaviour
 {
     #region Shooting Vars
     public float targetRange = 5f;
