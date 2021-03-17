2ba319f486ca11fbb36261c2cfd42f34a7217e3e
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-03-17 17:44:35 +0100

    :sparkles: Player switch

---
+++ Plantack/Assets/Scripts/Plantack/Player/PlayerUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Plantack.Player
+{
+    [CreateAssetMenu(fileName = "PlayerUI", menuName = "PlayerUI", order = 0)]
+    public class PlayerUI : ScriptableObject
+    {
+
+        
+        public RuntimeAnimatorController animator;
+        public Sprite defaultImage;
+        public Sprite jumpImage;
+        public Sprite runImage;
+        public Sprite attackImage;
+        public Sprite climbImage;
+
+
+    }
+}
---
+++ Plantack/Assets/Scripts/Plantack/Player/SetPlayerUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Plantack.Player
+{
+    public class SetPlayerUI : MonoBehaviour
+    {
+        [SerializeField] private PlayerUI[] playersUI;
+
+        [SerializeField] private Animator animator;
+        [SerializeField] private SpriteRenderer playerRenderer;
+        [SerializeField] private Image jumpButton;
+        [SerializeField] private Image climbButton;
+        [SerializeField] private Image runButton;
+        [SerializeField] private Image attackButton;
+
+        private PlayerUI _playerUI;
+        private int _playerIndex;
+        private void Start()
+        {
+            _playerIndex = PlayerPrefs.GetInt("playerIndex", 0);
+            _playerUI = playersUI[_playerIndex];
+            
+            
+            animator.runtimeAnimatorController = _playerUI.animator;
+            playerRenderer.sprite = _playerUI.defaultImage;
+            jumpButton.sprite = _playerUI.jumpImage;
+            climbButton.sprite = _playerUI.climbImage;
+            runButton.sprite = _playerUI.runImage;
+            if (attackButton != null)
+                //Sin implementar
+            {
+                attackButton.sprite = _playerUI.attackImage;
+            }
+        }
+    }
+}
